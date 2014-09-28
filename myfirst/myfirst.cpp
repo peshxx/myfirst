715862488972b837b6bbe0c2c04679520ef6fd38
ps1987 <pengshuo1987@gmail.com>
2014-09-28 17:36:01 +0800

    test

--- myfirst/myfirst.cpp
+++ myfirst/myfirst.cpp
@@ -50,24 +50,6 @@ vector<string> split(const string& str)
 	return ret;
 }
 
-// vector<Student_info> extract_fails(vector<Student_info>& students)
-// {
-// 	vector<Student_info> pass,fail;
-// 
-// 	for (vector<Student_info>::size_type i = 0;
-// 		i != students.size(); i++)
-// 	{
-// 			if (fgrade(students[i]))
-// 				fail.push_back(students[i]);
-// 			else
-// 				pass.push_back(students[i]);
-// 	}
-// 
-// 	students = pass;
-// 
-// 	return fail;
-// }
-
 int main(int, char **, char **)
 {
 	cout << "Please enter your first name: ";

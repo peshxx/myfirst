2a16c70bacdadc02dbf346be1e3dfb113a591575
ps1987 <pengshuo1987@gmail.com>
2014-10-23 17:29:30 +0800

    1

--- myfirst/myfirst.cpp
+++ myfirst/myfirst.cpp
@@ -52,39 +52,161 @@ vector<string> split(const string& str)
 	return ret;
 }
 
-int main(int, char **, char **)
+//int main(int, char **, char **)
+//{
+//	vector<Student_info> students;
+//	Student_info record;
+//
+//	string::size_type maxlen = 0;
+//
+//	while(read(cin, record))
+//	{
+//		maxlen = max(maxlen, record.name.size());
+//		students.push_back(record);
+//	}
+//
+//	sort(students.begin(), students.end(), compare);
+//
+//	for (vector<Student_info>::size_type i = 0; i!= students.size(); i++)
+//	{
+//		cout << setw(maxlen+1) << students[i].name;
+//
+//		try
+//		{
+//			double final_grade = grade(students[i]);
+//			streamsize prec = cout.precision();
+//			cout << setprecision(3) << final_grade
+//				<< setprecision(prec);
+//		}
+//		catch (domain_error e)
+//		{
+//			cout << e.what();
+//		}
+//		cout << endl;
+//
+//	}
+//
+//	return 0;
+//}
+
+struct free_throws
 {
-	vector<Student_info> students;
-	Student_info record;
+	string name;
+	int made;
+	int attempts;
+	float percent;
+};
 
-	string::size_type maxlen = 0;
+void display(const free_throws & ft);
 
-	while(read(cin, record))
+void set_pc(free_throws & ft);
+
+free_throws & accumulate(free_throws & target, const free_throws & source);
+
+//int main()
+//{
+//	free_throws one = {"Ifelsa Branch", 13, 14};
+//	free_throws two = {"Andor Knott", 10, 16};
+//	free_throws three = {"Minnie Max", 7, 9};
+//	free_throws four = {"Whily Looper", 5, 9};
+//	free_throws five = {"Long Long", 6, 14};
+//	free_throws team = {"Throwgoods", 0, 0};
+//	free_throws dups;
+//
+//	set_pc(one);
+//	display(one);
+//	accumulate(team, one);
+//	display(team);
+//
+//	display(accumulate(team, two));
+//	accumulate(accumulate(team,three), four);
+//	display(team);
+//
+//	dups = accumulate(team, five);
+//	cout << "Displaying team:\n";
+//	display(team);
+//
+//	cout << "Displaying dups after accumulate:\n";
+//	display(dups);
+//	set_pc(four);
+//
+//	accumulate(dups, five) = four;
+//	cout << "Disaplaying dups after 1ll-advesed assignment:\n"<<endl;
+//	display(dups);
+//	
+//	return 0;
+//}
+
+
+void display(const free_throws & ft)
+{
+	cout << "Name: " << ft.name << "\n";
+	cout << "Made: " << ft.made << "\t";
+	cout << "Attempts: " << ft.attempts << "\t";
+	cout << "Precent: " << ft.percent << "\n";
+}
+
+void set_pc(free_throws &ft)
+{
+	if (ft.attempts != 0)
 	{
-		maxlen = max(maxlen, record.name.size());
-		students.push_back(record);
+		ft.percent = 100.0f * float (ft.made) / float (ft.attempts);
 	}
-
-	sort(students.begin(), students.end(), compare);
-
-	for (vector<Student_info>::size_type i = 0; i!= students.size(); i++)
+	else
 	{
-		cout << setw(maxlen+1) << students[i].name;
-
-		try
-		{
-			double final_grade = grade(students[i]);
-			streamsize prec = cout.precision();
-			cout << setprecision(3) << final_grade
-				<< setprecision(prec);
-		}
-		catch (domain_error e)
-		{
-			cout << e.what();
-		}
-		cout << endl;
-
+		ft.percent = 0;
 	}
+}
+
+free_throws & accumulate(free_throws & target, const free_throws & source)
+{
+	target.attempts += source.attempts;
+	target.made += source.made;
+	set_pc(target);
+	return target;
+}
+
+
+const int ArSize = 80;
+char * left(const char * str, int n = 1);
+
+int main()
+{
+	using namespace std;
+	char sample[ArSize];
+	cout << "Enter a string\n";
+
+	cin.get(sample, ArSize);
+
+	char *ps = left(sample, 4);
+
+	cout << ps << endl;
+	delete [] ps;
+
+	ps = left(sample);
+	cout << ps << endl;
+
+	delete [] ps;
 
 	return 0;
+
+}
+
+char * left(const char * str, int n)
+{
+	if (n < 0)
+	{
+		n = 0;
+	}
+
+	char * p = new char[n+1];
+	int i;
+
+	for (i = 0; i < n && str[i]; i++)
+		p[i] = str[i];
+
+	while (i <= n)
+		p[i++] = '\0';
+
+	return p;
 }
